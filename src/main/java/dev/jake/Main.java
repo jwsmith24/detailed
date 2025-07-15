5037bca6d10e21a2685734eb60b5bc53cf0a45c9
Jake <jwsmit24@asu.edu>
2025-07-14 21:23:06 -0500

    add detail documentation and roster field

--- src/main/java/dev/jake/Main.java
+++ src/main/java/dev/jake/Main.java
@@ -1,7 +1,13 @@
 package dev.jake;
 
+import dev.jake.entities.Detail;
+import dev.jake.util.DetailType;
+
 public class Main {
     public static void main(String[] args) {
+
+        Detail staffDuty = new Detail(DetailType.SD);
+        Detail cq = new Detail(DetailType.CQ);
 
     }
 }
--- src/main/java/dev/jake/entities/Detail.java
+++ src/main/java/dev/jake/entities/Detail.java
@@ -1,8 +1,38 @@
 package dev.jake.entities;
 
 import dev.jake.util.DetailType;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * A detail object represents a DA Form 6 for a specific duty with the exception that it is a
+ * continuous, living document so that it's not required to make a new one for each month and
+ * carry over Soldier data. Incoming and outgoing Soldiers can simply be added or removed from
+ * the roster by the detail manager.
+ */
 public class Detail {
-    private DetailType type;
+    private final DetailType type;
     private String description; // include relevant info such as location, important POCs
+    private final List<Soldier> roster;
+
+    public Detail (DetailType type){
+        this.type = type;
+        this.roster = new ArrayList<Soldier>();
+    }
+
+    public DetailType getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Soldier> getRoster() {
+        return roster;
+    }
 }
